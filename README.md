1b789a89e5013fb592fbee48f48eaf75d0cde61a
eonliu <eonliu1024@gmail.com>
2022-01-24 14:02:00 +0800

    上传到maven central

---
+++ README.md
@@ -0,0 +1,3 @@
+# packer
+
+Packer是Android打包工具插件，支持360加固、多渠道打包、将APK上传到FTP等功能。
---
+++ packer-plugin/src/main/groovy/com/eonliu/packer/task/PackTask.groovy
@@ -0,0 +1,13 @@
+package com.eonliu.packer.task
+
+/**
+ * step1: 加固
+ * step2: 重新签名
+ * step3: 打多渠道包
+ * step4: 将多渠道包上传到ftp指定目录
+ * @author Yong Liu
+ */
+class PackTask extends BaseTask {
+
+
+}
