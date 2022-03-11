5194791feaa298fea5cf97d158d0d5d6685f5821
eon <eonliu1024@gmail.com>
2022-03-11 17:57:20 +0800

    v2.0.0 支持美团walle多渠道打包

--- packer-demo/src/main/java/com/eonliu/packer/demo/MainActivity.kt
+++ packer-demo/src/main/java/com/eonliu/packer/demo/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.eonliu.packer.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.meituan.android.walle.WalleChannelReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 通过美团Walle获取渠道信息
+        val channel = WalleChannelReader.getChannel(this.applicationContext)
+        Log.d("Packer", "Packer channel : $channel")
     }
 }
